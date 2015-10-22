13b1d0325a68163b0183a5c42978f0fa575a9827
Claude Falguiere <cfalguiere@gmail.com>
2015-10-22 23:28:28 +0200

    correction de la tonalité

--- 03-Exemples/BM-Sirene-de-pompier/programme/JouerUneNote/JouerUneNote.ino
+++ 03-Exemples/BM-Sirene-de-pompier/programme/JouerUneNote/JouerUneNote.ino
@@ -2,15 +2,41 @@
 // Ce programme joue une note
 
 int PIN_BUZZER = 8;
+const int DUREE = 500;
+const int DUREE_PAUSE = 0;
+
+const float DO = 261.625;
+const float RE = 293.664;
+const float MI = 329.627;
+const float FA = 349.228;
+const float SOL = 391.995;
+const float LA = 440.000;
+const float SI = 493.883;
+
+const float samu[] = { LA, FA };
+const float police[] = { LA, RE };
+const float pompiers[] = { LA, SI };
+const float ambulance[] = { LA, SOL, LA, 0 };
+
 
 // Cette partie n'est faite qu'une fois quand on appuie sur le bouton RESET de l'Arduino
 void setup () {                
-  int hauteur = 440;
-  tone(PIN_BUZZER, hauteur, 1000L);
-  delay(1000);               
 }
 
 // Cette partie est faite en boucle tant que l'on n'arrete pas l'arduino
 void loop () {
+  /*
+  // 
+  tone(PIN_BUZZER, LA, DUREE);
+  delay(DUREE);               
+  tone(PIN_BUZZER, SI, DUREE);
+  delay(DUREE);               
+  */
+  
+  // meme code en tableau
+  for (int i=0; i<2; i++) {
+    tone(PIN_BUZZER, pompiers[i], DUREE);
+    delay(DUREE);               
+  }
 }
 
