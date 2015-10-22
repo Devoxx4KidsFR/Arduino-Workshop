228ad62f3fffedcc5757922dc1f011bf3505ffff
Claude Falguiere <cfalguiere@gmail.com>
2015-10-22 23:28:50 +0200

    correction mineure

--- 03-Exemples/BL-Feu-Pietons/programme/FeuPietons/FeuPietons.ino
+++ 03-Exemples/BL-Feu-Pietons/programme/FeuPietons/FeuPietons.ino
@@ -1,20 +1,27 @@
 /*
-  fait clignoter une LED
+  Feu pietons
+  Allume alernativement la LED rouge et la LED verte
  */
 
+int LED_ROUGE = 13;
+int LED_VERTE = 12;
+int DUREE = 3000; // 3 secondes
 
 // Cette fonction est utilisee quand on demarre l'Arduino
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  pinMode(LED_ROUGE, OUTPUT);
+  pinMode(LED_VERTE, OUTPUT);
 }
 
 // cette fonction est appellee en boucle
 void loop() {
-  digitalWrite(13, HIGH);   // allume la LED   
-  delay(1000); // attends 1 seconde  
-  digitalWrite(13, LOW);  // eteint la LED
-  delay(1000); // attends 1 seconde  
+  digitalWrite(LED_ROUGE, HIGH);   // allume la LED rouge 
+  digitalWrite(LED_VERTE, LOW);   // eteint la LED verte  
+  delay(DUREE); // attends     
+  digitalWrite(LED_ROUGE, LOW);   // eteint la LED rouge 
+  digitalWrite(LED_VERTE, HIGH);   // allume la LED verte  
+  delay(DUREE); // attends 1 seconde  
 }
 
 
